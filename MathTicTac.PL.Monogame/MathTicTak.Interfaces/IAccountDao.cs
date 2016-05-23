ffe78bcaf44595e61033f46e7b4f8156adcc2abf
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-23 15:53:29 +0300

    Beginning

---
+++ MathTicTac.PL.Monogame/MathTicTak.Interfaces/IAccountDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTicTak.Interfaces
+{
+    public interface IAccountDao
+    {
+
+    }
+}
