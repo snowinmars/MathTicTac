047fa1746fe095848dd1c8d521f62e9bd4e4a177
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-26 21:55:44 +0300

    v0.3

---
+++ MathTicTac/MathTicTac.DAL.Dao/GameDao.cs
@@ -0,0 +1,33 @@
+﻿using MathTicTac.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathTicTac.DTO;
+
+namespace MathTicTac.DAL.Dao
+{
+    public class GameDao : IGameDao
+    {
+        public bool Add(DetailedWorld input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DetailedGameInfo> GetAllGames(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DetailedWorld GetGameState(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(DetailedWorld gameWorld)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
