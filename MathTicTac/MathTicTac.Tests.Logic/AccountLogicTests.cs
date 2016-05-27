db3b971a98baaa341ab51c44013e5b1ca0820e39
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-27 12:50:57 +0300

    New version

--- MathTicTac/MathTicTac.Tests.Logic/AccountLogicTests.cs
+++ MathTicTac/MathTicTac.Tests.Logic/AccountLogicTests.cs
@@ -17,18 +17,34 @@ namespace MathTicTac.Tests.Logic
             Mock<IAccountDao> mock = new Mock<IAccountDao>();
 
             mock.Setup(f 
-                => f.Add(It.IsAny<Account>(), It.IsAny<byte[]>())).Returns<Account, byte[]>((x, y) 
-                => 
-                {
+                => f.Add(It.IsAny<Account>(), It.IsAny<byte[]>()))
+                .Returns<Account, byte[]>((x, y) 
+                => {
                     x.Id = 13;
                     return true;
+                });
+            mock.Setup(f 
+                => f.Get(It.IsAny<int>()))
+                .Returns<int>(x
+                => {
+                    if (x == 13)
+                    {
+                        return new Account()
+                        {
+                            Id = 13,
+                            Username = "snow"
+                        };
+                    }
+
+                    return null;
                 });
 
             this.accountLogic = new AccountLogic(mock.Object);
         }
 
         [Theory]
-        [InlineData("Samir", "")]
+        [InlineData("Samir", "Pass")]
+        [InlineData("pr0gy", "Pass")]
         public void AddingNewCorrectUserMustReturnTrueAndId(string name, string password)
         {
             Account newUser = new Account();
@@ -38,6 +54,23 @@ namespace MathTicTac.Tests.Logic
 
             Assert.Equal<int>(13, newUser.Id);
             Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(13, "snow")]
+        [InlineData(15, null)]
+        public void GettingAccountById(int id, string res)
+        {
+            var result = accountLogic.Get(id);
+
+            if (res != null)
+            {
+                Assert.Equal<string>(res, result.Username);
+            }
+            else
+            {
+                Assert.True(result == null);
+            }
         }
     }
 }
