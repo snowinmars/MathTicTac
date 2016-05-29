9ddc2a38300c4bf8f8021a8d919aa8b758e8d480
snowonmars <stuff4work@yandex.ru>
2016-05-29 13:57:53 +0300

    Added Ninject config file

---
+++ MathTicTac/MathTicTac.PL.RestService/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,59 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MathTicTac.PL.RestService.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(MathTicTac.PL.RestService.App_Start.NinjectWebCommon), "Stop")]
+
+namespace MathTicTac.PL.RestService.App_Start
+{
+	using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+	using Ninject;
+	using Ninject.Web.Common;
+	using NinjectKernel;
+	using System;
+	using System.Web;
+
+	public static class NinjectWebCommon
+	{
+		private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+		public static IKernel Kernel;
+
+		/// <summary>
+		/// Starts the application
+		/// </summary>
+		public static void Start()
+		{
+			DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+			DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+			bootstrapper.Initialize(CreateKernel);
+		}
+
+		/// <summary>
+		/// Stops the application.
+		/// </summary>
+		public static void Stop()
+		{
+			bootstrapper.ShutDown();
+		}
+
+		/// <summary>
+		/// Creates the kernel that will manage your application.
+		/// </summary>
+		/// <returns>The created kernel.</returns>
+		private static IKernel CreateKernel()
+		{
+			var kernel = new StandardKernel();
+			try
+			{
+				kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+				kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+				Initializer.RegisterServices(kernel);
+				NinjectWebCommon.Kernel = kernel;
+				return kernel;
+			}
+			catch
+			{
+				kernel.Dispose();
+				throw;
+			}
+		}
+	}
+}
