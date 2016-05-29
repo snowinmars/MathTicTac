4e2c43f7009088f8081debe24b4f820c5e60593a
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-29 20:38:25 +0300

    First commit

--- MathTicTac/MathTicTac.Tests.Logic/AccountLogicTests.cs
+++ MathTicTac/MathTicTac.Tests.Logic/AccountLogicTests.cs
@@ -46,7 +46,7 @@ namespace MathTicTac.Tests.Logic
 
             mock.Setup(f
                 => f.GetUserPassword(It.Is<int>(x => x == 13)))
-                .Returns(new byte[] { (byte)110, (byte)148, (byte)203, (byte)180, (byte)130, (byte)40, (byte)207, (byte)26, (byte)229, (byte)46, (byte)54, (byte)234, (byte)188, (byte)182, (byte)147, (byte)155, (byte)53, (byte)11, (byte)124, (byte)180, (byte)59, (byte)185, (byte)232, (byte)130, (byte)226, (byte)198, (byte)105, (byte)69, (byte)161, (byte)182, (byte)158, (byte)37, (byte)73, (byte)172, (byte)3, (byte)140, (byte)151, (byte)188, (byte)40, (byte)113, (byte)85, (byte)100, (byte)57, (byte)216, (byte)189, (byte)121, (byte)126, (byte)155, (byte)106, (byte)130, (byte)113, (byte)94, (byte)165, (byte)196, (byte)246, (byte)245, (byte)30, (byte)208, (byte)147, (byte)142, (byte)141, (byte)139, (byte)78, (byte)156 });
+                .Returns(new byte[] { 110, 148, 203, 180, 130, 40, 207, 26, 229, 46, 54, 234, 188, 182, 147, 155, 53, 11, 124, 180, 59, 185, 232, 130, 226, 198, 105, 69, 161, 182, 158, 37, 73, 172, 3, 140, 151, 188, 40, 113, 85, 100, 57, 216, 189, 121, 126, 155, 106, 130, 113, 94, 165, 196, 246, 245, 30, 208, 147, 142, 141, 139, 78, 156 });
             mock.Setup(f
                 => f.GetUserPassword(It.Is<int>(x => x != 13)))
                 .Returns<byte[]>(null);
@@ -61,6 +61,13 @@ namespace MathTicTac.Tests.Logic
             mock.Setup(f
                 => f.CreateToken(It.Is<int>(x => x != 13), It.IsAny<string>()))
                 .Returns<string>(null);
+
+            mock.Setup(f
+                => f.AcceptToken(It.Is<string>(x => x == "058F39A9-420B-4F22-9689-47E99BD7E876")))
+                .Returns(DateTime.Now.AddDays(-5));
+            mock.Setup(f
+                => f.AcceptToken(It.Is<string>(x => x != "058F39A9-420B-4F22-9689-47E99BD7E876")))
+                .Returns<DateTime?>(null);
 
             this.accountLogic = new AccountLogic(mock.Object);
         }
@@ -99,13 +106,24 @@ namespace MathTicTac.Tests.Logic
         }
 
         [Theory]
-
+        // Correct data
         [InlineData("snow", "Pass", "192.168.0.1", "058F39A9-420B-4F22-9689-47E99BD7E876")]
+        // Incorrect data
+        [InlineData("pr0gy", "pass", "192.168.0.1", null)]
         public void LoggingByIdPassIp(string identifier, string password, string ip, string testRes)
         {
             var result = accountLogic.Login(identifier, password, ip);
 
             Assert.Equal<string>(testRes, result);
+        }
+
+        [Theory]
+        [InlineData("058F39A9-420B-4F22-9689-47E99BD7E876", "192.168.0.1")]
+        public void LoggingByToken(string token, string ip)
+        {
+            var result = accountLogic.Login(token, ip);
+
+            Assert.True(result);
         }
     }
 }
