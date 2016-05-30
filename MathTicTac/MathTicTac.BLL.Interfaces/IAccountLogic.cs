cfa451c55c8afc729e89a189fc6331eb1eb702df
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-05-30 15:54:23 +0300

    Enum changes

--- MathTicTac/MathTicTac.BLL.Interfaces/IAccountLogic.cs
+++ MathTicTac/MathTicTac.BLL.Interfaces/IAccountLogic.cs
@@ -1,23 +1,24 @@
 ﻿using MathTicTac.DTO;
+using MathTicTac.Enums;
 
 namespace MathTicTac.BLL.Interfaces
 {
 	public interface IAccountLogic
 	{
-		bool Add(Account item, string password);
+		ResponseResult Add(Account item, string password);
 
-		Account Get(int id);
+        ResponseResult Get(int id, string token, string ip, out Account account);
 
-		/// <summary>
-		/// Login user.
-		/// </summary>
-		/// <param name="identifier"></param>
-		/// <param name="password"></param>
-		/// <returns>User's token</returns>
-		string Login(string identifier, string password, string ip);
+        /// <summary>
+        /// Login user.
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <param name="password"></param>
+        /// <returns>User's token</returns>
+        ResponseResult Login(string identifier, string password, string ip, out string token);
 
-		bool Login(string token, string ip);
+        ResponseResult Login(string token, string ip);
 
-		bool Logout(string token, string ip);
+        ResponseResult Logout(string token, string ip);
 	}
 }
--- MathTicTac/MathTicTac.BLL.Interfaces/IGameLogic.cs
+++ MathTicTac/MathTicTac.BLL.Interfaces/IGameLogic.cs
@@ -1,27 +1,28 @@
 ﻿using MathTicTac.DTO;
+using MathTicTac.Enums;
 using System.Collections.Generic;
 
 namespace MathTicTac.BLL.Interfaces
 {
 	public interface IGameLogic
 	{
-		/// <summary>
-		/// Determinates, is player can make move to cell move.BigCellCoord and move.CellCoord
-		/// If return true, server and client have to make this move.
-		/// </summary>
-		/// <param name="move"></param>
-		/// <returns>true if move has been accepted, false if not (because of unknown error) or Auth exception if token and ip pair is invalid</returns>
-		bool MakeMove(Move move);
+        /// <summary>
+        /// Determinates, is player can make move to cell move.BigCellCoord and move.CellCoord
+        /// If return true, server and client have to make this move.
+        /// </summary>
+        /// <param name="move"></param>
+        /// <returns>true if move has been accepted, false if not (because of unknown error) or Auth exception if token and ip pair is invalid</returns>
+        ResponseResult MakeMove(Move move);
 
-		bool RejectGame(string token, string ip, int gameId);
+        ResponseResult RejectGame(string token, string ip, int gameId);
 
-		bool Create(string player1Token, string player1Ip, string player2Identifier);
+        ResponseResult Create(string player1Token, string player1Ip, string player2Identifier);
 
-		/// <summary>
-		/// Returns world copy from server for user with token token
-		/// </summary>
-		/// <returns></returns>
-		World GetCurrentWorld(string token, string ip, int gameId);
+        /// <summary>
+        /// Returns world copy from server for user with token token
+        /// </summary>
+        /// <returns></returns>
+        ResponseResult GetCurrentWorld(string token, string ip, int gameId, out World result);
 
 		IEnumerable<GameInfo> GetAllActiveGames(string token, string ip);
 	}
